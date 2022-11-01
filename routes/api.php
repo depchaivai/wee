1d007b630ce87ce5bdd657151c10a1489852c35a
tran <tri.tranthien@hdwebsoft.dev>
2022-11-01 09:32:21 +0700

    ft

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/image',function (Request $request){
+    $file = $request->file('image');
+    $uploadedFileUrl = $uploadedFileUrl = Cloudinary::upload($file)->getSecurePath();
+    return $uploadedFileUrl;
+});
